f05174a57451e7922805c1eea664ad785bfeac81
al-shaimon <alshaimon968@gmail.com>
2022-10-30 00:04:23 +0600

    Create Login component and style Login form

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUJbHne6s28VELwbUcXpeJixodx6NLST8",
+  authDomain: "ema-john-simple-4b684.firebaseapp.com",
+  projectId: "ema-john-simple-4b684",
+  storageBucket: "ema-john-simple-4b684.appspot.com",
+  messagingSenderId: "149415511608",
+  appId: "1:149415511608:web:5742507193926272bfc8cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
