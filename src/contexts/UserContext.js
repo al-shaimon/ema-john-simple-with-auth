98eed5661e31fb6bb232e680f891023b61be3ea9
al-shaimon <alshaimon968@gmail.com>
2022-10-30 23:13:49 +0600

    Implement Login Sign Up and Sign Out

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createContext } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import app from '../firebase/firebase.config';
 import { useState } from 'react';
 
@@ -11,7 +11,19 @@ const auth = getAuth(app);
 const UserContext = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  const authInfo = { user };
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  const logOut = () => {
+    return signOut(auth);
+  }
+
+  const authInfo = { user, createUser, signIn, logOut };
 
   return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
