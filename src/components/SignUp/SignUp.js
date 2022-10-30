390935b9581b3a2ae1d70b5674851e16c24bc7c7
al-shaimon <alshaimon968@gmail.com>
2022-10-30 21:06:56 +0600

    Get Form Data and display form error

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,27 +1,55 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './SignUp.css'
+import './SignUp.css';
 
 const SignUp = () => {
+
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirm = form.confirm.value;
+    
+    if(password.length < 6){
+      setError('Password should be 6 characters or more.')
+      return;
+    }
+
+    if(password !== confirm){
+      setError('Your Password did not match')
+      return;
+    }
+
+  };
+
   return (
-    <div className='form-container'>
-      <h2 className='form-title'>Sign Up</h2>
-      <form>
-          <div className="form-control">
-            <label htmlFor="email">Email</label>
-            <input type="email" name='email' required/>
-          </div>
-          <div className="form-control">
-            <label htmlFor="password">Password</label>
-            <input type="password" name='passowrd' required/>
-          </div>
-          <div className="form-control">
-            <label htmlFor="confirm">Confirm Password</label>
-            <input type="password" name='confirm' required/>
-          </div>
-          <input className='btn-submit' type="submit" value="Sign Up" />
+    <div className="form-container">
+      <h2 className="form-title">Sign Up</h2>
+      
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirm">Confirm Password</label>
+          <input type="password" name="confirm" required />
+        </div>
+        <p className='text-error'>{error}</p>
+        <input className="btn-submit" type="submit" value="Sign Up" />
       </form>
-      <p>Already Have an Account?<Link to='/login'>Login</Link></p>
+      <p>
+        Already Have an Account?<Link to="/login">Login</Link>
+      </p>
+      
     </div>
   );
 };
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { createContext } from 'react';
-
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import app from '../firebase/firebase.config';
+import { useState } from 'react';
 
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
 
-const UserContext = ({children}) => {
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-  const authInfo = {}
+  const authInfo = { user };
 
-  return (
-    <AuthContext.Provider value={authInfo}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
 
 export default UserContext;
